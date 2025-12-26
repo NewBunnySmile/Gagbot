6d7777b91fa1bc10b3ecc7293e8c0f94c5d3a2bc
dollliandra <dollliandra@gmail.com>
2025-12-25 21:35:05 -0500

    Shhhh~

--- gags/tape.js
+++ gags/tape.js
@@ -64,10 +64,10 @@ const garbleText = (text, intensity) => {
             let isUppercase = (textParts[x][itr] != textParts[x][itr].toLowerCase())
             if(tapeCharMap.get(textParts[x][itr].toLowerCase())){
                 // First letter is capital unless intensity 10. All other letters remain capital if intensity 1-6.
-                nextChar = isUppercase && ((itr == 0 && intensity < 10) || intensity < 7) ? tapeCharMap.get(textParts[x][itr].toLowerCase()).toUpperCase() : tapeCharMap.get(textParts[x][itr].toLowerCase())
+                nextChar = isUppercase && ((itr == 0 && intensity < 9) || intensity < 7) ? tapeCharMap.get(textParts[x][itr].toLowerCase()).toUpperCase() : tapeCharMap.get(textParts[x][itr].toLowerCase())
 
                 // Trash letters instead
-                if(Math.random() * 10 + 6 < intensity && itr != 0){nextChar = 'm'}
+                if((Math.random() * 10 + 6 < intensity) && itr != 0){nextChar = ''}
                 word += nextChar
 
                 // Intensity - Add duplicate chars randomly
@@ -131,35 +131,35 @@ exports.choicename = "Tape Gag"
 // Unit Tests
 
 //Test Gag Intensities
-let intensityTestMsg1   = "The quick brown fox jumps over the lazy dog."    // Classic pangram to test all letters.
-let intensityTestMsg2   = "HELP ME! This crazy doll is trying to turn me into one too!"
-let intensityTestMsg3   = "This unit is a good doll, and   will wear all possible tape gags for its Adminstrator."
-let intensityTestMsg4   = "Ha! I, in my brattiness, created  test-4, a test. . .  just to anger DOLL-0014 into domming me!!"
+// let intensityTestMsg1   = "The quick brown fox jumps over the lazy dog."    // Classic pangram to test all letters.
+// let intensityTestMsg2   = "HELP ME! This crazy doll is trying to turn me into one too!"
+// let intensityTestMsg3   = "This unit is a good doll, and   will wear all possible tape gags for its Adminstrator."
+// let intensityTestMsg4   = "Ha! I, in my brattiness, created  test-4, a test. . .  just to anger DOLL-0014 into domming me!!"
 
-console.log(`Original:          ${intensityTestMsg1}`)
-console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg1, 1)}`)
-console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg1, 3)}`)
-console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg1, 5)}`)
-console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg1, 7)}`)
-console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg1, 9)}`)
+// console.log(`Original:          ${intensityTestMsg1}`)
+// console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg1, 1)}`)
+// console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg1, 3)}`)
+// console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg1, 5)}`)
+// console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg1, 7)}`)
+// console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg1, 9)}`)
 
-console.log(`\nOriginal:          ${intensityTestMsg2}`)
-console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg2, 2)}`)
-console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg2, 4)}`)
-console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg2, 6)}`)
-console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg2, 8)}`)
-console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg2, 10)}`)
+// console.log(`\nOriginal:          ${intensityTestMsg2}`)
+// console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg2, 2)}`)
+// console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg2, 4)}`)
+// console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg2, 6)}`)
+// console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg2, 8)}`)
+// console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg2, 10)}`)
 
-console.log(`\nOriginal:          ${intensityTestMsg3}`)
-console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg3, 2)}`)
-console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg3, 4)}`)
-console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg3, 6)}`)
-console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg3, 8)}`)
-console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg3, 10)}`)
+// console.log(`\nOriginal:          ${intensityTestMsg3}`)
+// console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg3, 2)}`)
+// console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg3, 4)}`)
+// console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg3, 6)}`)
+// console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg3, 8)}`)
+// console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg3, 10)}`)
 
-console.log(`\nOriginal:          ${intensityTestMsg4}`)
-console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg4, 2)}`)
-console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg4, 4)}`)
-console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg4, 6)}`)
-console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg4, 8)}`)
-console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg4, 10)}`)
+// console.log(`\nOriginal:          ${intensityTestMsg4}`)
+// console.log(`Intensity 1-2:     ${garbleText(intensityTestMsg4, 2)}`)
+// console.log(`Intensity 3-4:     ${garbleText(intensityTestMsg4, 4)}`)
+// console.log(`Intensity 5-6:     ${garbleText(intensityTestMsg4, 6)}`)
+// console.log(`Intensity 7-8:     ${garbleText(intensityTestMsg4, 8)}`)
+// console.log(`Intensity 9-10:    ${garbleText(intensityTestMsg4, 10)}`)
