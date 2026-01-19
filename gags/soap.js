b4e194a1628ea7fdd7e913e1c81cbde28890b209
Enraa <enraaaery@gmail.com>
2026-01-19 13:15:02 -0800

    hehe

--- gags/soap.js
+++ gags/soap.js
@@ -23,14 +23,17 @@ const garbleText = (text, intensity) => {
         "crap",
         "hell",
         "cunt",
-        "bollocks"
+        "bollocks",
+        "slut",
+        "sluts",
+        "idiot"
 	];
 
     let cursemap = curses.join("|");
 	let regexpattern = new RegExp(`\\b(${cursemap})\\b`, "gi");
-    let textout = text.replaceAll(regexpattern, "🧼🧼🧼🧼")
+    let textout = text.replaceAll(regexpattern, ("🧼".repeat(Math.floor(intensity / 2))))
 
-	return textout;
+	return { text: textout, garble: true };
 };
 
 exports.garbleText = garbleText;
