8f589f23e18fb1bd0dbe82d21e8e0d0b1ba128df
Enraa <enraaaery@gmail.com>
2026-03-25 20:35:21 -0700

    ribbon

--- functions/wearablefunctions.js
+++ functions/wearablefunctions.js
@@ -90,12 +90,12 @@ let wearabletypes = [
 	{ name: "Doll Barcode", value: "cyberdoll_barcode", category: "Doll" },
 
 	//Faux Bondage and Non Restrictive Gear
-	{ name: "Harness", value: "harness_leather", colorable: true, category: "Bondage", uniqueColors: ["Vine", "Leather", "Latex", "Leather", "Rubber", "Shibari"] },
-	{ name: "Bondage Wrist Cuffs", value: "cuffswrist_bondage", colorable: true, category: "Bondage", uniqueColors: ["Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
+	{ name: "Harness", value: "harness_leather", colorable: true, category: "Bondage", uniqueColors: ["Ribbon", "Vine", "Leather", "Latex", "Leather", "Rubber", "Shibari"] },
+	{ name: "Bondage Wrist Cuffs", value: "cuffswrist_bondage", colorable: true, category: "Bondage", uniqueColors: ["Ribbon", "Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
 	{ name: "Latex Wrist Cuffs", value: "cuffswrist_latex", colorable: true, category: "Bondage", uniqueColors: ["Starry", "Shadow"] },
-	{ name: "Bondage Ankle Cuffs", value: "cuffsankle_bondage", colorable: true, category: "Bondage", uniqueColors: ["Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
+	{ name: "Bondage Ankle Cuffs", value: "cuffsankle_bondage", colorable: true, category: "Bondage", uniqueColors: ["Ribbon", "Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
 	{ name: "Latex Ankle Cuffs", value: "cuffsankle_latex", colorable: true, category: "Bondage", uniqueColors: ["Starry", "Shadow"] },
-	{ name: "Bondage Thigh Cuffs", value: "cuffsthigh_bondage", colorable: true, category: "Bondage", uniqueColors: ["Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
+	{ name: "Bondage Thigh Cuffs", value: "cuffsthigh_bondage", colorable: true, category: "Bondage", uniqueColors: ["Ribbon", "Hardlight", "Steel", "Golden", "Leather", "Rubber", "Cursed"] },
 	{ name: "Latex Thigh Cuffs", value: "cuffsthigh_latex", colorable: true, category: "Bondage", uniqueColors: ["Starry", "Shadow"] },
 	{ name: "Cyber Doll Cuffs", value: "cuffs_cyberdoll", colorable: true, category: "Bondage" },
 	//{ name: "Latex Legbinder", value: "legbinder_latex", colorable: true, category: "Bondage", uniqueColors: ["Starry", "Shadow"] },
