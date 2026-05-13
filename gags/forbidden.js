b2a8ae50cd19968521fce02f011ca54c80f798a1
Enraa <enraaaery@gmail.com>
2026-05-12 18:04:53 -0700

    Rework Forbidden to use Regex
    It was just simple string searches before.

--- gags/forbidden.js
+++ gags/forbidden.js
@@ -2,20 +2,25 @@ const { getOption } = require("../functions/configfunctions");
 
 const garbleText = (text, parent, intensity, msg) => {
 	let newtextparts = text.split(" ");
-	let outtext = "";
+	let outtext = text;
     let forbiddenwords = getOption(msg.author.id, "forbiddengagpunishwords") ?? [];
-	for (let i = 0; i < newtextparts.length; i++) {
-		forbiddenwords.forEach((w) => {
-            if (newtextparts[i].toLowerCase() === w.toLowerCase()) {
-                let forbiddenstars = `✦`;
-                for (let a = 0; a < newtextparts[i].length; a++) {
-                    forbiddenstars = `${forbiddenstars}✦`
-                }
-                newtextparts[i] = forbiddenstars
+    console.log(forbiddenwords)
+    if (!Array.isArray(forbiddenwords)) { return outtext } // Just skip this gag if it's not an array.
+    forbiddenwords.forEach((w) => {
+        let regexcomp = new RegExp(`\\b(${w})\\b`, "gi");
+        let replacement = ``;
+        let loopcount = 0;
+        let loopmax = 100;
+        for (let a = 0; a < w.length; a++) {
+            replacement = `${replacement}✦`
+        }
+        if (outtext.match(regexcomp)) {
+            while ((outtext.match(regexcomp)) && (loopcount < loopmax)) {
+                outtext = outtext.replace(regexcomp, replacement)
+                loopcount++;
             }
-        })
-	}
-    outtext = newtextparts.join(" ")
+        }
+    })
 	return outtext;
 }
 
