7c47bf58502859246c2e3aaac7c02dfaafce622f
Enraa <enraaaery@gmail.com>
2026-05-13 20:12:17 -0700

    I oopsied

--- eventfunctions/gags/headpatslut.js
+++ eventfunctions/gags/headpatslut.js
@@ -3,15 +3,15 @@ const { getUserVar, setUserVar } = require("../../functions/usercontext");
 
 // Successful headpats will recharge the battery on the recipient's vibe by 5%. Each minute drains 2%. 
 function headpatfunction(recipient, headpatter, returnedobject) {
-    setUserVar(recipient, "headpatslutgag", Date.now() + (300000 * getOption(recipient, "headpatrestraintpotency")));
+    if (returnedobject.hit) {
+        setUserVar(recipient, "headpatslutgag", Date.now() + (300000 * getOption(recipient, "headpatrestraintpotency")));
+    }
 }
 
 // Update battery
 async function functiontick(userid) {
-    if (returnedobject.hit) {
-        if (getUserVar(userid, "headpatslutgag") && (getUserVar(userid, "headpatslutgag") < Date.now())) {
-            setUserVar(userid, "headpatslutgag", undefined);
-        }
+    if (getUserVar(userid, "headpatslutgag") && (getUserVar(userid, "headpatslutgag") < Date.now())) {
+        setUserVar(userid, "headpatslutgag", undefined);
     }
 }
 
